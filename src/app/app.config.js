78e6136df0ff1557ccfbbff6041cfc049e90a19c
Kristijan Horvat <kristijan@mono.hr>
2015-06-03 16:05:21 +0200

    Using the AngularJS Filters

--- src/app/app.config.js
+++ src/app/app.config.js
@@ -3,10 +3,12 @@
 	angular.module('todoList')
 		.config(['$routeProvider',
 			function ($routeProvider) {
+			/* 
 			$routeProvider
 				.when('/', { templateUrl: 'app/todo-list/list.html', title: 'ToDo List' })
 				.when('/insert', { templateUrl: 'app/todo-list/insert.html', title: 'Insert ToDo' })
 				.when('/preview', { templateUrl: 'app/todo-list/preview.html', title: 'Preview ToDo' })
 				.otherwise({ redirectTo: '/' });
+			*/
 		}]);	
 })(angular);
