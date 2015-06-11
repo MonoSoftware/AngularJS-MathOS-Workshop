4e42f35f2212abd0870e84510ebf86aa2aca6d0d
Kristijan Horvat <kristijan@mono.hr>
2015-06-11 17:32:11 +0200

    Custom directive templating - templateUrl sample

--- src/app/todo-list/todo-list-preview.directive.js
+++ src/app/todo-list/todo-list-preview.directive.js
@@ -9,29 +9,7 @@
 					scope: {
 	                    selectedTodo: '='
 	                },
-					template: "<ul>" +
-"    <li>" +
-"        Id:{{selectedTodo.id}}" +        
-"    </li>" +
-"    <li>" +
-"        Title:{{selectedTodo.title}}" +        
-"    </li>" +
-"    <li>" +
-"        Upper:{{selectedTodo.title | uppercase}}" +        
-"    </li>" +
-"    <li>" +
-"        Lower:{{selectedTodo.title | lowercase}}" +      
-"    </li>" +
-"    <li>" +
-"        Date:{{selectedTodo.date | date:'yyyy-MM-dd hh:mm'}}" +        
-"    </li>" +
-"    <li>" +
-"        Done:{{selectedTodo.done}}" +        
-"    </li>" +
-"    <li>" +
-"        Textual done:{{selectedTodo.done | doneToText}}" +        
-"    </li>" +
-"</ul>"	                
+					templateUrl: 'app/todo-list/todo-list-preview.html' 	                
 				}
 			 }]);
 })(angular);
